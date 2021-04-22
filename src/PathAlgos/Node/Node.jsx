46dd90a311ed034c85321682cba51cbbcfa78e09
Rajkishor Pradhan <rajp7598@gmail.com>
2021-04-22 18:18:24 +0530

    Grid id and click works good

--- src/PathAlgos/Node/Node.jsx
+++ src/PathAlgos/Node/Node.jsx
@@ -4,7 +4,7 @@ import "./Node.css";
 
 // export default class Node extends Component {
 //     render() {
-//         const { col, isFinish, isStart, isWall, onMouseDown, onMouseEnter, onMouseUp, row } = this.props;
+//         const { col, isFinish, isStart, isWall, onMouseDown, onMouseEnter, onMouseUp, row } = props;
 //         const extraClassName = isFinish ? "node-finish" : isStart ? "node-start" : isWall ? "node-wall" : "";
 
 //         return (
@@ -20,11 +20,19 @@ import "./Node.css";
 // }
 
 function Node(props) {
+    // const { col, isFinish, isStart, isWall, onMouseDown, onMouseEnter, onMouseUp, row } = props;
+    const extraClassName = props.isFinish
+        ? "node-finish"
+        : props.isStart
+        ? "node-start"
+        : props.isWall
+        ? "node-wall"
+        : "";
     return (
         <div
             id={`node-${props.row}-${props.col}`}
-            className={`node ${props.extraClassName}`}
-            // onMouseDown={() => onMouseDown(props.row, props.col)}
+            className={"node " + extraClassName}
+            onMouseDown={() => props.mouseDown(props.row, props.col)}
             // onMouseEnter={() => onMouseEnter(props.row, props.col)}
             // onMouseUp={() => onMouseUp()}
         ></div>
--- src/PathAlgos/Pathfinder.jsx
+++ src/PathAlgos/Pathfinder.jsx
@@ -15,14 +15,48 @@ function Pathfinder() {
     });
 
     useEffect(() => {
-        const grid = getInitialGrid();
-        setgridAndMouse({ grid });
-    }, [getInitialGrid]);
+        function createNode(col, row) {
+            // console.log(col, row);
+            return (
+                <Node
+                    col={col}
+                    row={row}
+                    isStart={row === START_NODE_ROW && col === START_NODE_COL}
+                    isFinish={row === FINISH_NODE_ROW && col === FINISH_NODE_COL}
+                    distance={Infinity}
+                    isVisited={false}
+                    isWall={false}
+                    previousNode={null}
+                />
+            );
+        }
+
+        function getInitialGrid() {
+            const grid = [];
+            for (let row = 0; row < 19; row++) {
+                const currentRow = [];
+                for (let col = 0; col < 50; col++) {
+                    currentRow.push(createNode(col, row));
+                }
+                grid.push(currentRow);
+            }
+            setgridAndMouse({
+                grid: grid
+            });
+        }
+
+        getInitialGrid();
+        // const grid = getInitialGrid();
+    }, []);
 
     // function handleMouseDown(row, col) {
-    //     const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
-    //     this.setState({ grid: newGrid, mouseIsPressed: true });
+    //     const newGrid = getNewGridWithWallToggled(gridAndMouse.grid, row, col);
+    //     setgridAndMouse({ grid: newGrid, mouseIsPressed: true });
     // }
+    function handleMouseDown(row, col) {
+        const newGrid = getNewGridWithWallToggled(gridAndMouse.grid, row, col);
+        setgridAndMouse({ grid: newGrid, mouseIsPressed: true });
+    }
 
     // function handleMouseEnter(row, col) {
     //     if (!this.state.mouseIsPressed) return;
@@ -34,31 +68,16 @@ function Pathfinder() {
         setgridAndMouse({ mouseIsPressed: false });
     }
 
-    function getInitialGrid() {
-        const grid = [];
-        for (let row = 0; row < 19; row++) {
-            const currentRow = [];
-            for (let col = 0; col < 50; col++) {
-                currentRow.push(createNode(col, row));
-            }
-            grid.push(currentRow);
-        }
-        return grid;
-    }
-
-    function createNode(col, row) {
-        return (
-            <Node
-                col={col}
-                row={row}
-                isStart={row === START_NODE_ROW && col === START_NODE_COL}
-                isFinish={row === FINISH_NODE_ROW && col === FINISH_NODE_COL}
-                distance={Infinity}
-                isVisited={false}
-                isWall={false}
-                previousNode={null}
-            />
-        );
+    function getNewGridWithWallToggled(grid, row, col) {
+        const newGrid = grid.slice();
+        const node = newGrid[row][col];
+        console.log(node);
+        const newNode = {
+            ...node,
+            [node.props.isWall]: !node.props.isWall
+        };
+        newGrid[row][col] = newNode;
+        return newGrid;
     }
 
     return (
@@ -71,15 +90,15 @@ function Pathfinder() {
                             return (
                                 <Node
                                     key={nodeIdx}
-                                    col={col}
-                                    isFinish={isFinish}
-                                    isStart={isStart}
-                                    isWall={isWall}
+                                    col={nodeIdx}
+                                    isStart={rowIdx === START_NODE_ROW && nodeIdx === START_NODE_COL}
+                                    isFinish={rowIdx === FINISH_NODE_ROW && nodeIdx === FINISH_NODE_COL}
+                                    isWall={false}
                                     mouseIsPressed={gridAndMouse.mouseIsPressed}
-                                    // onMouseDown={(row, col) => this.handleMouseDown(row, col)}
+                                    mouseDown={handleMouseDown}
                                     // onMouseEnter={(row, col) => this.handleMouseEnter(row, col)}
                                     // onMouseUp={() => this.handleMouseUp()}
-                                    row={row}
+                                    row={rowIdx}
                                 ></Node>
                             );
                         })}
